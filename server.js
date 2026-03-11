93770651b16356fbf7c60f489543d196009d7e06
Kevin thapa <bobmarley9461238@gmail.com>
2026-03-11 20:57:39 +0000

    Usage of readFileSync at the top to read the whole data.json file for persistent data whilst using process.argv to take input from the terminal

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ if (!fs.existsSync("data.json")) {
     fs.writeFileSync("data.json", JSON.stringify([]))
     console.log("data.json has been created!")
 }
+
+const reading = JSON.parse(fs.writeFileSync("data.json", "utf-8"))
+
+const args = process.argv
+const method = args[2]
+
+
